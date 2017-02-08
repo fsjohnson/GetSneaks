d4573eae39eb3f113f89a26cb9e087b0ceb5971b
fjohnson1991 <felicity.johnson.mail@gmail.com>
2017-02-07 20:00:22 -0800

    first commit

---
+++ Memories/Memories/Memories/InitialView.swift
@@ -0,0 +1,21 @@
+//
+//  InitialView.swift
+//  Memories
+//
+//  Created by Felicity Johnson on 2/7/17.
+//  Copyright © 2017 FJ. All rights reserved.
+//
+
+import UIKit
+
+class InitialView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
